e6cc4d5a926fa7dcaaf087c092dd15b1a9fbba5e
Utku Cerit <utct@protonmail.com>
2025-02-16 20:20:16 -0500

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={"/seg/"}></BrowserRouter>
     <App />
   </StrictMode>,
 )
